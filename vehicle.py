11714152353f838409154a5e50cea6b7809ab254
Programmer <Programmer@ua>
2024-12-16 17:06:54 +0200

    паттерн Фабричний метод

--- vehicle.py
+++ vehicle.py
@@ -1,38 +1,80 @@
+# pattern Builder
+from abc import ABC, abstractmethod
+
+
 class Vehicle:
-    def __init__(self, vehicle_type, max_speed, fuel_capacity, fuel_efficiency, extras=None):
-        self.vehicle_type = vehicle_type
-        self.max_speed = max_speed
-        self.fuel_capacity = fuel_capacity
-        self.fuel_efficiency = fuel_efficiency
-        self.extras = extras or []
+    # def __init__(self, vehicle_type, max_speed, fuel_capacity, fuel_efficiency, extras=None):
+    #     self.vehicle_type = vehicle_type
+    #     self.max_speed = max_speed
+    #     self.fuel_capacity = fuel_capacity
+    #     self.fuel_efficiency = fuel_efficiency
+    #     self.extras = extras or []
+    #
+    # def __str__(self):
+    #     return (f"{self.vehicle_type}: Max Speed={self.max_speed}, Fuel Capacity={self.fuel_capacity}, "
+    #             f"Fuel Efficiency={self.fuel_efficiency}, Extras={self.extras}")
+
+    def __init__(self):
+        self.features = []
+
+    def add_feature(self, feature):
+        self.features.append(feature)
 
     def __str__(self):
-        return (f"{self.vehicle_type}: Max Speed={self.max_speed}, Fuel Capacity={self.fuel_capacity}, "
-                f"Fuel Efficiency={self.fuel_efficiency}, Extras={self.extras}")
+        return ', '.join(self.features)
 
 
-class VehicleBuilder:
+class VehicleBuilder(ABC):
     def __init__(self):
-        self.vehicle_type = None
-        self.max_speed = None
-        self.fuel_capacity = None
-        self.fuel_efficiency = None
-        self.extras = []
+        self.vehicle = None
 
-    def set_vehicle_type(self, vehicle_type):
-        self.vehicle_type = vehicle_type
+    def create_new_vehicle(self):
+        self.vehicle = Vehicle()
 
-    def set_max_speed(self, max_speed):
-        self.max_speed = max_speed
+    @abstractmethod
+    def set_vehicle_type(self):
+        pass
 
-    def set_fuel_capacity(self, fuel_capacity):
-        self.fuel_capacity = fuel_capacity
+    @abstractmethod
+    def set_max_speed(self):
+        pass
 
-    def set_fuel_efficiency(self, fuel_efficiency):
-        self.fuel_efficiency = fuel_efficiency
+    @abstractmethod
+    def set_fuel_capacity(self):
+        pass
 
-    def add_extra(self, extra):
-        self.extras.append(extra)
+    @abstractmethod
+    def set_fuel_efficiency(self):
+        pass
 
-    def build(self):
-        return Vehicle(self.vehicle_type, self.max_speed, self.fuel_capacity, self.fuel_efficiency, self.extras)
+    @abstractmethod
+    def add_extra(self):
+        pass
+
+    def get_vehicle(self):
+        return self.vehicle
+
+    # def __init__(self):
+    #     self.vehicle_type = None
+    #     self.max_speed = None
+    #     self.fuel_capacity = None
+    #     self.fuel_efficiency = None
+    #     self.extras = []
+    #
+    # def set_vehicle_type(self, vehicle_type):
+    #     self.vehicle_type = vehicle_type
+    #
+    # def set_max_speed(self, max_speed):
+    #     self.max_speed = max_speed
+    #
+    # def set_fuel_capacity(self, fuel_capacity):
+    #     self.fuel_capacity = fuel_capacity
+    #
+    # def set_fuel_efficiency(self, fuel_efficiency):
+    #     self.fuel_efficiency = fuel_efficiency
+    #
+    # def add_extra(self, extra):
+    #     self.extras.append(extra)
+    #
+    # def build(self):
+    #     return Vehicle(self.vehicle_type, self.max_speed, self.fuel_capacity, self.fuel_efficiency, self.extras)
